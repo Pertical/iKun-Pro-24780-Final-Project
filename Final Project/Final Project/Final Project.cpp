dd15be4af3ee51fdc68e783cd1523ca85d7f17d9
Pertical <zhufeiyapeter@gmail.com>
2022-11-29 19:43:30 -0500

    Update

--- Final Project/Final Project/Final Project.cpp
+++ Final Project/Final Project/Final Project.cpp
@@ -5,4 +5,41 @@
 //  Created by Peter Zhu on 11/29/22.
 //
 
-#include "Final Project.hpp"
+#include "yssimplesound.h"
+
+class soundtrack
+{
+public:
+    YsSoundPlayer player;
+    YsSoundPlayer::SoundData background;
+    soundtrack();
+};
+
+soundtrack::soundtrack()
+{
+    player.MakeCurrent();
+    player.Start();
+    if (YSOK != background.LoadWav("S"))
+    {
+        printf("Error!  Cannot load !\n");
+    }
+}
+
+
+int main(void)
+{
+    
+    
+    while(1)
+    {
+        
+        
+        
+        
+        
+    }
+    return 0;
+    
+    
+    
+}
