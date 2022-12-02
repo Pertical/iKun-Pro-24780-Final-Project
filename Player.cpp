d4165cbe2bc20900611775867f9004ea763e55c2
Vinzental <85374546+vinzental15@users.noreply.github.com>
2022-12-02 15:10:08 -0500

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -20,6 +20,7 @@ void Player::drawPlayer(double x, double y) {
 	glEnd();
 }
 void Player::Healthbar(int hx, int hy, int fill) {
+	
 	if (0 != fill) {
 		glColor3ub(255, 0, 0);
 		glBegin(GL_POLYGON);
@@ -36,6 +37,35 @@ void Player::Healthbar(int hx, int hy, int fill) {
 	}
 	glEnd();
 }
+
+void Player::DrawHealthbar(int hx, int hy, int health) {
+	int hx2 = hx + 30;
+	int hx3 = hx2 + 30;
+
+	if (health == 3) {
+		Healthbar(hx, hy, 1);
+		Healthbar(hx2, hy, 1);
+		Healthbar(hx3, hy, 1);
+	}
+	if (health == 2) {
+		Healthbar(hx, hy, 1);
+		Healthbar(hx2, hy, 1);
+		Healthbar(hx3, hy, 0);
+	}
+	if (health == 1) {
+		Healthbar(hx, hy, 1);
+		Healthbar(hx2, hy, 0);
+		Healthbar(hx3, hy, 0);
+	}
+	if (health == 0) {
+		Healthbar(hx, hy, 0);
+		Healthbar(hx2, hy, 0);
+		Healthbar(hx3, hy, 0);
+	}
+}
+
+
+
 bool Player::CheckCollision(int mx, int my, int tx, int ty, int tw, int th)
 {
 	if ((mx + 20) >= tx && (mx + 20) <= tx + tw)
@@ -91,145 +121,4 @@ void Player::Move(int key, int& x, int& y) {
 		y = 350;
 	}
 }
-
-
-//Test target
-
-
-
-//void drawTarget(double xi, double yi, int sizex, int sizey)
-//{
-//	glColor3ub(255, 0, 0);
-//	glBegin(GL_QUADS);
-//	glVertex2i(xi, yi);
-//	glVertex2i(xi + sizex, yi);
-//	glVertex2i(xi + sizex, yi - sizey);
-//	glVertex2i(xi, yi - sizey);
-//
-//	glEnd();
-//}
-//
-//void moveTarget(double& x, double& y, int scrnsizex, int scrnsizey)
-//{
-//	double v = 1;
-//	x -= 2.5 * v; // 0.25m
-//	if ((x + 50) == scrnsizex)
-//	{
-//		x = 800.0;
-//		v += 0.5;
-//		printf("%lf", v);
-//	}
-//}
-
-
-	//int main(void)
-	//{
-	//	srand(time(NULL));
-	//	Player p;
-	//
-	//	int pState = 0; // 0 full health; 1 hitted; 
-	//	int x = 100, y = 450;
-	//	int hx = 50, hy = 50;
-	//	int hx2 = 80, hx3 = 110;
-	//	int health = 3;
-	//	int i = 0;
-	//
-	//	//Test target
-	//	double targetxi = 600.0, targetyi = 500.0;
-	//	int targetsizex = 50, targetsizey = 50;
-	//
-	//	FsOpenWindow(16, 16, 800, 600, 1);
-	//
-	//	for (;;)
-	//	{
-	//		p.Move(x, y);
-	//		/*FsPollDevice();
-	//		auto key = FsInkey();
-	//		if (FSKEY_ESC == key)
-	//		{
-	//			break;
-	//		}
-	//
-	//		if (FSKEY_LEFT == key)
-	//		{
-	//			x -= 10;
-	//		}
-	//		else if (FSKEY_RIGHT == key)
-	//		{
-	//			x += 10;
-	//		}
-	//		else if (FSKEY_DOWN == key)
-	//		{
-	//			y += 10;
-	//		}
-	//		else if (FSKEY_UP == key)
-	//		{
-	//			y -= 10;
-	//		}
-	//
-	//		if (x + 50 > 400) {
-	//			x = 350;
-	//		}
-	//		else if (x < 0) {
-	//			x = 0;
-	//		}
-	//		if (y > 600) {
-	//			y = 600;
-	//		}
-	//		else if (y - 50 < 300) {
-	//			y = 350;
-	//		}*/
-	//		//cout << x << endl;
-	//		//cout << y << endl;
-	//
-	//
-	//		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	//
-	//		p.drawBounds();
-	//
-	//		p.drawPlayer(x, y);
-	//		
-	//		//p.Healthbar(hx, hy, 0);
-	//		//Test
-	//		drawTarget(targetxi, targetyi, targetsizex, targetsizey);
-	//		moveTarget(targetxi, targetyi, 0, 600);
-	//		//
-	//		if (health == 3) {
-	//			p.Healthbar(hx, hy, 1);
-	//			p.Healthbar(hx2, hy, 1);
-	//			p.Healthbar(hx3, hy, 1);
-	//		}
-	//		if (health == 2) {
-	//			p.Healthbar(hx, hy, 1);
-	//			p.Healthbar(hx2, hy, 1);
-	//			p.Healthbar(hx3, hy, 0);
-	//		}
-	//		if (health == 1) {
-	//			p.Healthbar(hx, hy, 1);
-	//			p.Healthbar(hx2, hy, 0);
-	//			p.Healthbar(hx3, hy, 0);
-	//		}
-	//		if (health == 0) {
-	//			p.Healthbar(hx, hy, 0);
-	//			p.Healthbar(hx2, hy, 0);
-	//			p.Healthbar(hx3, hy, 0);
-	//		}
-	//
-	//		if (true == p.CheckCollision(x, y, targetxi, targetyi, targetsizex, targetsizey)) { // TO BE FIXED
-	//			i++;
-	//			//printf("1");
-	//		}
-	//
-	//		if (i > 7) {
-	//			health = health - 1;
-	//			i = 0;
-	//		}
-	//
-	//		FsSwapBuffers();
-	//		FsSleep(25);
-	//	}
-	//	return 0;
-	//}
-	//
-	//
 
